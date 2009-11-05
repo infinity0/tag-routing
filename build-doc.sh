268003e178705a2e88d98e468406c685cbce943a
Ximin Luo <xl269@cam.ac.uk>
2009-11-05 17:47:00 +0000

    buildscripts for documentation

---
+++ build-doc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Generate documents from source code
+
+SRC="src-doc"
+BLD="build-doc"
+DOC="doc"
+
+mkdir -p "$DOC"
+mkdir -p "$BLD"
+
+cd "$SRC"
+
+for i in *.tex; do
+	sed -e 's/\(\s\)"/\1``/g' "$i" > "../$BLD/$i" && pdflatex -output-directory "../$BLD" "$i"
+	cp "../$BLD/${i%.tex}.pdf" "../$DOC/${i%.tex}.pdf"
+done
+
+for i in *.md.txt; do
+	markdown "$i" > "../$DOC/${i%.md.txt}.html"
+done
+
+cd ..
+
+rm -rf "$BLD"
