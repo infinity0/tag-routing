f3ddadfb1d84a539d2eaa2987acff91472b1e3bb
Ximin Luo <xl269@cam.ac.uk>
2010-01-12 22:04:04 +0000

    package structure

---
+++ src/tags/proto/package-info.java
@@ -0,0 +1,5 @@
+// Released under GPLv2 or later. See http://www.gnu.org/ for details.
+/**
+** Prototype implementation.
+*/
+package tags.proto;
---
+++ src/tags/proto/route/package-info.java
@@ -0,0 +1,5 @@
+// Released under GPLv2 or later. See http://www.gnu.org/ for details.
+/**
+** Routing layer.
+*/
+package tags.proto.route;
