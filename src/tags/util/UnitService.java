9799aee1c8dea9db0c2dea64775d3cb00d709534
Ximin Luo <xl269@cam.ac.uk>
2010-01-17 22:10:25 +0000

    add UnitService class

---
+++ src/tags/util/UnitService.java
@@ -0,0 +1,58 @@
+// Released under GPLv2 or later. See http://www.gnu.org/ for details.
+package tags.util;
+
+import java.util.concurrent.Executor;
+
+/**
+** A service that runs a maximum of one concurrent job.
+*/
+public class UnitService {
+
+	protected Executor exec;
+
+	protected boolean active;
+	protected int completed;
+
+	public UnitService(Executor exec) {
+		setExecutor(exec);
+	}
+
+	public synchronized void setExecutor(Executor exec) {
+		if (exec == null) {
+			throw new NullPointerException();
+		}
+		this.exec = exec;
+	}
+
+	public synchronized boolean isActive() {
+		return active;
+	}
+
+	/**
+	** Schedule the given job.
+	**
+	** @return An identifier for the job (increments for every job completed)
+	** @throws IllegalStateException if a job is already running
+	*/
+	protected synchronized int start(final Runnable run) {
+		if (active) {
+			throw new IllegalStateException("already running a job");
+		}
+		exec.execute(new Runnable() {
+			@Override public void run() {
+				try {
+					run.run();
+				} catch (RuntimeException e) {
+					// FIXME LOW
+				}
+				synchronized(UnitService.this) {
+					++completed;
+					active = false;
+				}
+			}
+		});
+		active = true;
+		return completed;
+	}
+
+}
