6f98c90fda033bcc3c540ded1fe7e2c198e729a1
Ximin Luo <xl269@cam.ac.uk>
2010-01-18 17:41:35 +0000

    more methods for *TGraph

--- src/tags/proto/LocalTGraph.java
+++ src/tags/proto/LocalTGraph.java
@@ -2,6 +2,7 @@
 package tags.proto;
 
 import java.util.Set;
+import java.util.Map;
 
 /**
 ** Local view of a {@link TGraph}.
@@ -17,6 +18,26 @@ public class LocalTGraph<T, U, W> extends TGraph<T, U, W> {
 	** have all been loaded.
 	*/
 	public Set<T> getCompletedTags() {
+		throw new UnsupportedOperationException("not implemented");
+	}
+
+	public Set<T> getIncoming(T dst) {
+		throw new UnsupportedOperationException("not implemented");
+	}
+
+	public Map<T, U> getIncomingTagWeights(T dst) {
+		throw new UnsupportedOperationException("not implemented");
+	}
+
+	public Map<T, W> getIncomingArcWeights(T dst) {
+		throw new UnsupportedOperationException("not implemented");
+	}
+
+	public void setTagWeight(T tag, U wgt) {
+		throw new UnsupportedOperationException("not implemented");
+	}
+
+	public void setArcWeight(T src, T dst, W wgt) {
 		throw new UnsupportedOperationException("not implemented");
 	}
 
--- src/tags/proto/TGraph.java
+++ src/tags/proto/TGraph.java
@@ -1,6 +1,9 @@
 // Released under GPLv2 or later. See http://www.gnu.org/ for details.
 package tags.proto;
 
+import java.util.Set;
+import java.util.Map;
+
 /**
 ** Tag graph. DOCUMENT.
 **
@@ -9,5 +12,25 @@ package tags.proto;
 ** @param <W> Type of arc-attribute
 */
 public class TGraph<T, U, W> {
+
+	public U getTagWeight(T tag) {
+		throw new UnsupportedOperationException("not implemented");
+	}
+
+	public W getArcWeight(T src, T dst) {
+		throw new UnsupportedOperationException("not implemented");
+	}
+
+	public Set<T> getOutgoing(T src) {
+		throw new UnsupportedOperationException("not implemented");
+	}
+
+	public Map<T, U> getOutgoingTagWeights(T src) {
+		throw new UnsupportedOperationException("not implemented");
+	}
+
+	public Map<T, W> getOutgoingArcWeights(T src) {
+		throw new UnsupportedOperationException("not implemented");
+	}
 
 }
