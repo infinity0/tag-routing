10e505e0351320991d3adb6b2cc85d6a2b31e428
Ximin Luo <xl269@cam.ac.uk>
2010-03-08 02:52:36 +0000

    implement Tasks.newTask()

--- src/tags/util/exec/Tasks.java
+++ src/tags/util/exec/Tasks.java
@@ -9,8 +9,52 @@ final public class Tasks {
 	private Tasks() { }
 
 	public static <K> Task<K> newTask(K key) {
-		// TODO HIGH
-		throw new UnsupportedOperationException("not implemented");
+		return new BaseTaskResult<K, Object, Exception>(key);
+	}
+
+	public static class BaseTaskResult<K, V, X extends Exception> implements TaskResult<K, V, X> {
+
+		final public K key;
+		protected V val;
+		protected X err;
+		protected boolean done;
+
+		public BaseTaskResult(K key) {
+			this.key = key;
+		}
+
+		@Override public K getKey() {
+			return key;
+		}
+
+		@Override public V getValue() throws X {
+			if (err == null) {
+				return val;
+			} else {
+				throw err;
+			}
+		}
+
+		@Override public X getError() {
+			return err;
+		}
+
+		@Override public boolean isDone() {
+			return done;
+		}
+
+		public void setValue(V val) {
+			if (done) { throw new IllegalStateException("already done"); }
+			this.val = val;
+			done = true;
+		}
+
+		public void setError(X err) {
+			if (done) { throw new IllegalStateException("already done"); }
+			this.err = err;
+			done = true;
+		}
+
 	}
 
 }
