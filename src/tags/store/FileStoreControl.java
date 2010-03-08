6cd554ef184887d65ceab1e9c394dba44a0b7b4b
Ximin Luo <xl269@cam.ac.uk>
2010-03-08 12:13:59 +0000

    make FileStoreControl use memory data instead of trying to read from files becuase i need to have something to show for supervision in 2 hours ¬.¬

--- src/tags/store/FileStoreControl.java
+++ src/tags/store/FileStoreControl.java
@@ -7,6 +7,7 @@ import tags.proto.Index;
 import tags.util.Union.U2;
 import tags.util.Maps.U2Map;
 import java.util.Map;
+import java.util.HashMap;
 
 import java.io.File;
 import java.io.IOException;
@@ -26,6 +27,12 @@ public class FileStoreControl<I, T, A, U, W, S, Z> implements StoreControl<I, T,
 
 	final public File basedir;
 
+	final public Map<I, Map<I, Z>> map_fr = new HashMap<I, Map<I, Z>>();
+	final public Map<I, PTable<A, S>> map_ptab = new HashMap<I, PTable<A, S>>();
+	final public Map<A, Map<T, U2Map<T, A, W>>> map_tgr = new HashMap<A, Map<T, U2Map<T, A, W>>>();
+	final public Map<A, U2Map<T, A, U>> map_tgr_node = new HashMap<A, U2Map<T, A, U>>();
+	final public Map<A, Map<T, U2Map<A, A, W>>> map_idx = new HashMap<A, Map<T, U2Map<A, A, W>>>();
+
 	/**
 	** @throws IllegalArgumentException if {@code basedir} is not a directory
 	*/
@@ -41,28 +48,62 @@ public class FileStoreControl<I, T, A, U, W, S, Z> implements StoreControl<I, T,
 	}
 
 	public Map<I, Z> getFriends(I id) throws IOException {
-		File fp = getFile(id + ".fr");
-		throw new UnsupportedOperationException("not implemented");
+		Map<I, Z> fr;
+
+		fr = map_fr.get(id);
+		if (fr == null) { throw new IOException(); }
+
+		//File fp = getFile(id + ".fr");
+		//throw new UnsupportedOperationException("not implemented");
+		return fr;
 	}
 
 	public PTable<A, S> getPTable(I id) throws IOException {
-		File fp = getFile(id + ".ptab");
-		throw new UnsupportedOperationException("not implemented");
+		PTable<A, S> ptab;
+
+		ptab = map_ptab.get(id);
+		if (ptab == null) { throw new IOException(); }
+
+		//File fp = getFile(id + ".ptab");
+		//throw new UnsupportedOperationException("not implemented");
+
+		return ptab;
 	}
 
 	public U2Map<T, A, W> getTGraphOutgoing(A addr, T src) throws IOException {
-		File fp = getFile(addr + ".tgr");
-		throw new UnsupportedOperationException("not implemented");
+		Map<T, U2Map<T, A, W>> tgr;
+
+		tgr = map_tgr.get(addr);
+		if (tgr == null) { throw new IOException(); }
+
+		//File fp = getFile(addr + ".tgr");
+		//throw new UnsupportedOperationException("not implemented");
+
+		return tgr.get(src);
 	}
 
 	public U getTGraphNodeAttr(A addr, U2<T, A> node) throws IOException {
-		File fp = getFile(addr + ".tgr");
-		throw new UnsupportedOperationException("not implemented");
+		U2Map<T, A, U> tgr_node;
+
+		tgr_node = map_tgr_node.get(addr);
+		if (tgr_node == null) { throw new IOException(); }
+
+		//File fp = getFile(addr + ".tgr");
+		//throw new UnsupportedOperationException("not implemented");
+
+		return tgr_node.get(node);
 	}
 
 	public U2Map<A, A, W> getIndexOutgoing(A addr, T src) throws IOException {
-		File fp = getFile(addr + ".idx");
-		throw new UnsupportedOperationException("not implemented");
+		Map<T, U2Map<A, A, W>> idx;
+
+		idx = map_idx.get(addr);
+		if (idx == null) { throw new IOException(); }
+
+		//File fp = getFile(addr + ".idx");
+		//throw new UnsupportedOperationException("not implemented");
+
+		return idx.get(src);
 	}
 
 	protected File getFile(String s) {
